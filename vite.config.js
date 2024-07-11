e6e1c11f0df29a3dafc03758bb6aa5d2776851b9
TMedalith <u20211f955@upc.edu.pe>
2024-07-11 12:57:02 -0500

    firtcommit

---
+++ vite.config.js
@@ -0,0 +1,13 @@
+import vue from '@vitejs/plugin-vue'
+import { defineConfig } from 'vite'
+import { templateCompilerOptions } from '@tresjs/core'
+
+export default defineConfig({
+  plugins: [
+    vue({
+      template: {
+        compilerOptions: templateCompilerOptions
+      }
+    })
+  ]
+})
